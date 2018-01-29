0c7c18950931c576773041105374c5dfeb9ea861
Warren Woolsey <warren.woolseyiii@gmail.com>
2018-01-29 13:35:44 -0800

    Got the new unit test project compiling and running. Getting everything hooked up in visual studio is a little more complicated. Had to point the unit test project to the static library project directory to find the header file. Then I had to add a reference to the static library project. Finally I had to change everything to x86 (which it has to be anyway) because the unit test process is a 32 bit executable.

--- Trace Logger Cpp/Trace Logger Cpp Tests/TraceLoggerUnitTests.cpp
+++ Trace Logger Cpp/Trace Logger Cpp Tests/TraceLoggerUnitTests.cpp
@@ -1,21 +1,25 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "string.h"
+#include "TraceLogger.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Trace_Logger_Cpp;
 
 namespace TraceLoggerCppTests
 {		
 	TEST_CLASS(TraceLoggerUnitTests)
 	{
 	public:
-		
+		string moduleName = "TraceLoggerTests::TestBuildTraceLoggerMessage()";
 		TEST_METHOD(TestBuildTraceLoggerMessage)
 		{
+			string methodName = "TraceLoggerTests::TestBuildTraceLoggerMessage()";
+			string message = "TraceLoggerTests::TestBuildTraceLoggerMessage()";
+
 			TraceLogger logger;
 			TraceLoggerMessage msg = logger.BuildMessage(moduleName, methodName, message);
-			AssertAreEqualStrings(moduleName, msg.moduleName, "TraceLoggerTests::TestBuildTraceLoggerMessage()", 1);
-			AssertAreEqualStrings(methodName, msg.methodName, "TraceLoggerTests::TestBuildTraceLoggerMessage()", 2);
-			AssertAreEqualStrings(message, msg.message, "TraceLoggerTests::TestBuildTraceLoggerMessage()", 3);
+			Assert::IsFalse(moduleName.compare(msg.moduleName));
 		}
 
 	};
